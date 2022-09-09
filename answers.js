6f77d74407158ef5e897471993504d07d991dbc6
arsampson33 <arsampson33@gmail.com>
2022-09-08 22:11:10 -0400

    answered wild wild life

--- answers.js
+++ answers.js
@@ -20,3 +20,17 @@ for (let i = 0; i <= 100; i++){
         console.log(i)
     }
 }
+
+//Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2]++
+wolfy[3] = 'Gotham City'
+dart.push('Hawkins')
+wolfy.shift()
+wolfy.unshift('Gameboy')
+console.log(wolfy)
