ab52dea8d64a940bb61d12d40ae1866cb3db9b4d
arsampson33 <arsampson33@gmail.com>
2022-09-08 22:02:43 -0400

    answered fizzbuzz

--- answers.js
+++ answers.js
@@ -7,3 +7,16 @@ for (let i = 0; i<=20; i++){
 for (let i = 0; i <= 200; i+=2){
     console.log(i)
 }
+
+//Fizz Buzz
+for (let i = 0; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz')
+    } else if (i % 3=== 0){
+         console.log('Fizz')
+    } else if (i % 5 === 0){
+        console.log('Buzz')
+    } else {
+        console.log(i)
+    }
+}
